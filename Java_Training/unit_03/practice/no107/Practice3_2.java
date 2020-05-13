bb4285c5a63a00e215c883e20bfcae5f56d1dfbf
u-misawa <u.misawa@systemi.co.jp>
2020-05-13 18:17:18 +0900

    フォルダ名の変更

---
+++ Java_Training/unit_03/practice/no107/Practice3_2.java
@@ -0,0 +1,13 @@
+package Java_Training.unit_03.practice.no107;
+
+public class Practice3_2 {
+	public static void main(String args[]){
+		String value = null;
+
+		if (value == null) {
+			System.out.println("値なし");
+		} else {
+			System.out.println(value);
+		}
+	}
+}
---
+++ Java_Training/unit_04/practice/no107/practice4_2.java
@@ -0,0 +1,21 @@
+package Java_Training.unit_04.practice.m107;
+
+public class practice4_2 {
+
+	public static void main(String[] args){
+
+		for(int i = 1; i < 10; i++){
+			for(int j = 1; j < 10; j++){
+				System.out.print(j * i + " ");
+
+				if (9 == j){
+					System.out.println();
+				}
+			}
+			
+
+			
+		}
+	}
+	
+}
