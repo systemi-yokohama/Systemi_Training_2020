fc10d043e1c9d8d05e00ebfc8b8f8b48db03bad5
s-kikuchi1 <s.kikuchi@systemi.co.jp>
2020-05-15 11:10:05 +0900

    Java研修unit1~4

---
+++ Java_Training/unit_02/practice/no215/Practice2-5.md
@@ -0,0 +1,10 @@
+1.　var value = 10d;  
+2.　System.out.println("ようこそ、\nJavaの世界へ！");  
+3.　String str = null;  
+4.　int[][] data = new data[5][4];  
+5.　  
+var list = new int[][] {  
+{ 2,3,5 },  
+{ 1,2 },  
+{ 10,11,12,13 }  
+} ;
---
+++ Java_Training/unit_04/practice/no215/Practice4_2.java
@@ -0,0 +1,20 @@
+package Java_Training.unit_04.practice.no215;
+
+public class Practice4_2 {
+
+	public static void main(String[] args){
+		int i=1;
+		int sum=0;
+
+	while(i<=100){
+		sum += i;
+		if(sum > 1000){
+			break;
+		}
+		i++;
+	}
+	System.out.println("合計が1000を超えるのは、1~"+ i +"を加算したときです。");
+
+	}
+	
+}
