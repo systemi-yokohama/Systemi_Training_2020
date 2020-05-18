dad4eb26b7a2e8a9079ec8ede61649ab5a40b564
s-inoue1108 <s.inoue@systemi.co.jp>
2020-05-18 15:08:00 +0900

    削除した

--- Java_Training/unit_03/practice/no107/Practice3_2.java
+++ Java_Training/unit_03/practice/no107/Practice3_2.java
@@ -1,14 +1,29 @@
-// package Java_Training.unit_03.practice.no107;
+package Java_Training.unit_03.practice.no107;
 
-// public class Practice3_2 {
-// 	public static void main(String args[]) {
-// 		String value = null;
+public class Practice3_2 {
+	public static void main(String[] args) {
+		String value = null;
 
-// 		if (value == null) {
-// 			System.out.println("値なし");
-// 		} else {
-// 			System.out.println(value);
-// 		}
+		if (value == null) {
+			System.out.println("値なし");
+		} else {
+			System.out.println(value);
+		}
 
-// 	}
-// }
+		var x = 6;
+		var y = x;
+		y -= 2;
+
+		var builder1 = new StringBuilder("いろは");
+		var builder2 = builder1;
+
+		builder1.append("にほへと");
+
+		System.out.println(x);
+		System.out.println(y);
+
+		System.out.println(builder1);
+		System.out.println(builder2);
+
+	}
+}
