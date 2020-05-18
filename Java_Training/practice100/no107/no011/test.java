ca24ed5d0b4e9a4fbf1c50a3f6ccf99a2680d3b0
s-inoue1108 <s.inoue@systemi.co.jp>
2020-05-18 09:57:58 +0900

    理解度チェック

--- Java_Training/practice100/no107/no011/test.java
+++ Java_Training/practice100/no107/no011/test.java
@@ -1,4 +1,4 @@
-package Java_Training.100practice.no107.no011;
+package Java_Training.practice100.no107.no011;
 
 public class test {
 
--- Java_Training/unit_03/practice/no107/Practice3_2.java
+++ Java_Training/unit_03/practice/no107/Practice3_2.java
@@ -1,14 +1,14 @@
-package Java_Training.unit_03.practice.no107;
+// package Java_Training.unit_03.practice.no107;
 
-public class Practice3_2 {
-	public static void main(String args[]) {
-		String value = null;
+// public class Practice3_2 {
+// 	public static void main(String args[]) {
+// 		String value = null;
 
-		if (value == null) {
-			System.out.println("値なし");
-		} else {
-			System.out.println(value);
-		}
+// 		if (value == null) {
+// 			System.out.println("値なし");
+// 		} else {
+// 			System.out.println(value);
+// 		}
 
-	}
-}
+// 	}
+// }
