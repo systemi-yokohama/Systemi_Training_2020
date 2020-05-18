053f5cc61fb5823d053c01fa12b7d1e51e0e2d8a
s-kikuchi1 <s.kikuchi@systemi.co.jp>
2020-05-18 09:46:24 +0900

    Java unit5~6

---
+++ Java_Training/practice100/no215/no062.java
@@ -0,0 +1,8 @@
+package Java_Training.practice100.no215;
+
+public class no062 {
+	public static void main(String[] args){
+		var map=new HashMap<String,String>(Map.of());
+		for(var key:map Keyset()){
+			System.out.println(key);
+		}
