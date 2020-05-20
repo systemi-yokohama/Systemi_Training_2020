4072742b679262f7f80648491c206a9f479c7b17
h-kogushi <h.kogushi@systemi.co.jp>
2020-05-20 09:51:44 +0900

    チャプター8

--- Java_Training/practice100/no220/no049/No049.java
+++ Java_Training/practice100/no220/no049/No049.java
@@ -1,35 +1,47 @@
 package Java_Training.practice100.no220.no049;
 import java.util.*;
 import java.text.*;
+import java.time.*;
+
 
 public class No049 {
 
 	
 	public static void main(String[] args) {
-
-		Date date = new Date();
-		Calendar calendar = Calendar.getInstance();
-		SimpleDateFormat sdf = new SimpleDateFormat("yyyy/MM/dd/HH/mm/ss");
-		
-		var fdate = sdf.format(date);
-		
-		// 現在時刻
-		System.out.println(fdate);
-
-		// 一日前
-		calendar.add(Calendar.DAY_OF_MONTH, -1);
-		var yesterday = sdf.format(calendar.getTime());
-		System.out.println(yesterday);
-
-		// 一日後
-		calendar.add(Calendar.DAY_OF_MONTH, +2);
-		var tommorow = sdf.format(calendar.getTime());
-		System.out.println(tommorow);
-
-		
-		Random rand = new Random();
-		rand.nextInt(Integer.parseInt(tommorow) + yesterday);
-
-    }
+		public static List<String> createDateList8(int year, int month, int day, int count) {
+			List<String> list = new ArrayList<>();
+			
+			for (int i = 0; i < count; i++) {
+				long dayStart = LocalDate.of(year, month, day).getLong(ChronoField.EPOCH_DAY);
+				long dayEnd   = LocalDate.now().getLong(ChronoField.EPOCH_DAY);
 	
+				LocalDate dateRandom;
+				long dayPeriod;
+				if (dayStart <= dayEnd) {
+					dayPeriod = dayEnd - dayStart;
+					dateRandom = LocalDate.now().minusDays(new Random().nextInt((int) dayPeriod));
+				} else {
+					dayPeriod = dayStart - dayEnd;
+					dateRandom = LocalDate.now().plusDays(new Random().nextInt((int) dayPeriod));
+				}
+				
+				if (i == count -1) {
+					System.out.println("指定年月日の日数 : " + dayStart);
+					System.out.println("現在年月日の日数 : " + dayEnd);	
+					System.out.println("期間日数　 　　　: " + dayPeriod);
+					System.out.println("作成年月日 　　　: " + dateRandom);
+				}
+				
+				list.add(dateRandom.toString());
+			}
+			
+			return list;
+		}
+			
+		public static void main(String[] args) {
+			createDateList8(1997, 1, 1, 100);
+		}
+
+	}
 }
+	
