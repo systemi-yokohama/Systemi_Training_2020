2c5c25b578ab09c58b870eae8c4277c494ec6149
LIU-HONGYUAN <k.ryu@systemi.co.jp>
2020-05-22 12:02:48 +0900

    unit10

--- Java_Training/practice100/no219/No018.java
+++ Java_Training/practice100/no219/No018.java
@@ -1,27 +1,22 @@
 package Java_Training.practice100.no219;
 
-public class No018 {
-	public enum Junishi{
-		NE("子", 1), USHI("丑", 2), TORA("寅", 3), U("卯", 4), TATSU("辰", 5), MI("巳", 6),HITSUJI("未", 7), UMA("午", 8), SARU("申", 9), TORI("酉", 10), INU("戌", 11), I("亥", 12);
-		
-		private int id;
-		private String name;
-		
-		private Junishi(int id,String name){
-			this.id=id;
-			this.name=name;
-		}
-		public int toJunishiValue(){
-			return this.id;
-		}
-		@Override
-		public String toString(){
-			return this.name;
-		}
+public enum No018{
+	NE("子", 1), USHI("丑", 2), TORA("寅", 3), U("卯", 4), TATSU("辰", 5), MI("巳", 6),HITSUJI("未", 7), UMA("午", 8), SARU("申", 9), TORI("酉", 10), INU("戌", 11), I("亥", 12);
+	//????
+	private int id;
+	private String name;
+	
+	private No018(int id,String name){
+		this.id=id;
+		this.name=name;
+	}
+	public int toJunishiValue(){
+		return this.id;
+	}
+	@Override
+	public String toString(){
+		return this.name;
+	}
 
-    
-	}
-	public static void main(String[] args) {
-		System.out.println(Junishi.USHI);
-	}
+
 }
