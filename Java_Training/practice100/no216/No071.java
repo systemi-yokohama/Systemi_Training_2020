838f1314099a6ef5cfcaa5e0c0a9563721221cf0
runa-murakami <r.murakami@systemi.co.jp>
2020-05-26 09:01:51 +0900

    java演習_0525

---
+++ Java_Training/practice100/no216/No071.java
@@ -0,0 +1,36 @@
+package Java_Training.practice100.no216;
+
+public class No071 {
+	private static long longValue = 0;
+
+    public static void main(String[] args) throws Exception {
+        final int LOOP = 1000 * 1000;
+
+        // スレッド1で値をaddする
+        Thread th1 = new Thread(new Runnable() {
+            public void run() {
+                for (int i = 0; i < LOOP; i++) {
+                    longValue++;
+                }
+            }
+        });
+
+        // スレッド2でも値をaddする
+        Thread th2 = new Thread(new Runnable() {
+            public void run() {
+                for (int i = 0; i < LOOP; i++) {
+                    longValue++;
+                }
+            }
+        });
+
+        th1.start();
+        th2.start();
+
+        // 2つのスレッドが止まるまでまつ
+        th1.join();
+        th2.join();
+
+        System.out.println("Finished:" +  longValue);
+    }
+}
