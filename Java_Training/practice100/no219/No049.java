a0d378157c3694254f010f48b8c4dc5344d261fa
LIU-HONGYUAN <k.ryu@systemi.co.jp>
2020-05-26 10:47:45 +0900

    unit 11

--- Java_Training/practice100/no219/No049.java
+++ Java_Training/practice100/no219/No049.java
@@ -1,22 +1,13 @@
 package Java_Training.practice100.no219;
 
-import java.sql.Date;
-import java.time.LocalDate;
-import java.time.Period;
-import java.util.ArrayList;
-import java.util.HashSet;
-import java.util.List;
+import java.util.Date;
 
-import javax.xml.crypto.Data;
+import Java_Training.practice100.no211.DateUtil;
 
 public class No049 {
 	public static void main(String[] args) {
-
-		ArrayList<Date> l1 = new ArrayList<>();
-		l1.add(LocalDate.now());
-		l1.add(d.plus(period));
-		l1.add(d.minus(period));
-		l1=new ArrayList<>(new HashSet<>(l1));
-        
+		Date randomDate = new DateUtil().randomDayAndTime();
+		System.out.println(randomDate);
+		
 	}
 }
