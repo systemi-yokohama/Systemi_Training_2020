ab0f451956fa8ddcbeba9809bd068d2e8245237d
K-Maeda-git <k.maeda@systemi.co.jp>
2020-05-29 16:58:22 +0900

    SimpelTwitter

---
+++ WebProject/no217/src/main/java/LoginServlet.java
@@ -0,0 +1,52 @@
+// package chapter6.controller;
+
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
+
+import beans.User;
+import service.LoginService;
+
+@WebServlet(urlPatterns = { "/login" })
+public class LoginServlet extends HttpServlet {
+    private static final long serialVersionUID = 1L;
+
+    @Override
+    protected void doGet(HttpServletRequest request,
+            HttpServletResponse response) throws IOException, ServletException {
+
+        request.getRequestDispatcher("login.jsp").forward(request, response);
+    }
+
+    @Override
+    protected void doPost(HttpServletRequest request,
+            HttpServletResponse response) throws IOException, ServletException {
+
+        String accountOrEmail = request.getParameter("accountOrEmail");
+        String password = request.getParameter("password");
+
+        LoginService loginService = new LoginService();
+        User user = loginService.login(accountOrEmail, password);
+
+        HttpSession session = request.getSession();
+        if (user != null) {
+
+            session.setAttribute("loginUser", user);
+            response.sendRedirect("./");
+        } else {
+
+            List<String> messages = new ArrayList<String>();
+            messages.add("ログインに失敗しました。");
+            session.setAttribute("errorMessages", messages);
+            response.sendRedirect("login");
+        }
+    }
+
+}
