9d90ad84fb3cdbf362ccb3a5d98450115de6b861
s-inoue1108 <s.inoue@systemi.co.jp>
2020-05-29 18:30:22 +0900

    webproject

---
+++ WebProject/no218/src/main/java/mysql_sqlinjection_ass.java
@@ -0,0 +1,46 @@
+
+// import java.sql.Connection;
+// import java.sql.DriverManager;
+// import java.sql.SQLException;
+// import java.sql.Statement;
+
+// public class mysql_sqlinjection_ass {
+	
+// 	String driver = "com.mysql.jdbc.Driver"; 
+// 	String url = "jdbc:mysql://localhost/practice_db";
+// 	String user = "root";
+// 	String password = "root";
+
+//   Class.forName(driver);
+
+//   Connection connection = DriverManager.getConnection(url,
+//       user, password);
+//   connection.setAutoCommit(false);
+
+//   String deleteId = "6";
+
+//   delete(connection, deleteId);
+
+//   mysql_select.select(connection);
+
+//   connection.commit();
+//   connection.close();
+// }
+
+// public static void delete(Connection connection,
+//     String deleteId) throws SQLException {
+
+//   Statement statement = connection.createStatement();
+
+//   String sql = "DELETE FROM authors WHERE id = " + deleteId;
+//   int updateCount = statement.executeUpdate(sql);
+
+//   if (updateCount == 1) {
+//     System.out.println("削除成功");
+//   } else {
+//     System.out.println("削除失敗");
+//   }
+
+//   statement.close();
+// }
+// }
