0f7be40fa9fb77dd4f5e19c28fd312ca254bbc18
runa-murakami <r.murakami@systemi.co.jp>
2020-06-08 11:36:43 +0900

    0608_変更

---
+++ WebProject/no216/src/main/java/service/MessageService.java
@@ -0,0 +1,63 @@
+package service;
+
+import static utils.CloseableUtil.*;
+import static utils.DBUtil.*;
+
+import java.sql.Connection;
+import java.util.List;
+
+import beans.Message;
+import beans.UserMessage;
+import dao.MessageDao;
+import dao.UserMessageDao;
+
+
+public class MessageService {
+
+    public void register(Message message) {
+
+        Connection connection = null;
+        try {
+            connection = getConnection();
+
+            MessageDao messageDao = new MessageDao();
+            messageDao.insert(connection, message);
+
+            commit(connection);
+        } catch (RuntimeException e) {
+            rollback(connection);
+            throw e;
+        } catch (Error e) {
+            rollback(connection);
+            throw e;
+        } finally {
+            close(connection);
+        }
+    }
+
+    private static final int LIMIT_NUM = 1000;
+
+    public List<UserMessage> getMessage() {
+
+    	Connection connection = null;
+    	try {
+    		connection = getConnection();
+
+    		UserMessageDao messageDao = new UserMessageDao();
+    		List<UserMessage> ret = messageDao.getUserMessages(connection, LIMIT_NUM);
+
+    		commit(connection);
+
+    		return ret;
+    	} catch (RuntimeException e) {
+    		rollback(connection);
+    		throw e;
+    	} catch (Error e) {
+    		rollback(connection);
+    		throw e;
+    	} finally {
+    		close(connection);
+    	}
+    }
+
+}
---
+++ WebProject/no216/src/main/java/utils/DBUtil.java
@@ -0,0 +1,79 @@
+package utils;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
+import exception.SQLRuntimeException;
+
+/**
+ * DB(コネクション関係)のユーティリティー
+ */
+public class DBUtil {
+
+	private static final String DRIVER = "com.mysql.jdbc.Driver";
+	private static final String URL = "jdbc:mysql://localhost/simple_twitter";
+	private static final String USER = "root";
+	private static final String PASSWORD = "root";
+
+	static {
+
+		try {
+			Class.forName(DRIVER);
+		} catch (ClassNotFoundException e) {
+			throw new RuntimeException(e);
+		}
+	}
+
+	/**
+	 * コネクションを取得します。
+	 *
+	 * @return
+	 */
+	public static Connection getConnection() {
+
+		try {
+			Connection connection = DriverManager.getConnection(URL, USER,
+					PASSWORD);
+
+			connection.setAutoCommit(false);
+
+			return connection;
+		} catch (SQLException e) {
+			throw new SQLRuntimeException(e);
+		}
+	}
+
+	/**
+	 * コミットします。
+	 *
+	 * @param connection
+	 */
+	public static void commit(Connection connection) {
+
+		try {
+			connection.commit();
+		} catch (SQLException e) {
+			throw new SQLRuntimeException(e);
+		}
+	}
+
+	/**
+	 * ロールバックします。
+	 *
+	 * @param connection
+	 */
+	public static void rollback(Connection connection) {
+
+		if (connection == null) {
+			return;
+		}
+
+		try {
+			connection.rollback();
+		} catch (SQLException e) {
+			throw new SQLRuntimeException(e);
+		}
+	}
+
+}
