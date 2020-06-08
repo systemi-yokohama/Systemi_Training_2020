3108b2df0e94f85a40607f850357dee24c5912b1
s-kikuchi1 <s.kikuchi@systemi.co.jp>
2020-06-08 11:36:59 +0900

    web開発0608

--- WebProject/no215/src/main/java/Delete.java
+++ WebProject/no215/src/main/java/Delete.java
@@ -1,47 +1,47 @@
-import java.sql.Connection;
-import java.sql.DriverManager;
-import java.sql.SQLException;
-import java.sql.Statement;
+// import java.sql.Connection;
+// import java.sql.DriverManager;
+// import java.sql.SQLException;
+// import java.sql.Statement;
 
 
-public class Delete {
-	public static void main(String[] args) throws Exception {
-        String driver = "com.mysql.jdbc.Driver";
-        // 「database_name」は各環境に合わせます
-        String url = "jdbc:mysql://localhost/practice_db";
-        // MySQLに接続する際のユーザー名(デフォルトはroot)
-        String user = "root";
-        // MySQLに接続する際のパスワード(今回はroot)
-        String password = "saya3814";
+// public class Delete {
+// 	public static void main(String[] args) throws Exception {
+//         String driver = "com.mysql.jdbc.Driver";
+//         // 「database_name」は各環境に合わせます
+//         String url = "jdbc:mysql://localhost/practice_db";
+//         // MySQLに接続する際のユーザー名(デフォルトはroot)
+//         String user = "root";
+//         // MySQLに接続する際のパスワード(今回はroot)
+//         String password = "saya3814";
 
-        Class.forName(driver);
+//         Class.forName(driver);
 
-        Connection connection = DriverManager.getConnection(url,
-                user, password);
-        connection.setAutoCommit(false);
+//         Connection connection = DriverManager.getConnection(url,
+//                 user, password);
+//         connection.setAutoCommit(false);
 
-        delete(connection);
+//         delete(connection);
 
-        Select.select(connection);
+//         Select.select(connection);
 
-        connection.commit();
-        connection.close();
-    }
+//         connection.commit();
+//         connection.close();
+//     }
 
-    public static void delete(Connection connection)
-            throws SQLException {
+//     public static void delete(Connection connection)
+//             throws SQLException {
 
-        Statement statement = connection.createStatement();
+//         Statement statement = connection.createStatement();
 
-        String sql = "DELETE FROM authors WHERE id = 6";
-        int updateCount = statement.executeUpdate(sql);
+//         String sql = "DELETE FROM authors WHERE id = 6";
+//         int updateCount = statement.executeUpdate(sql);
 
-        if (updateCount == 1) {
-            System.out.println("削除成功");
-        } else {
-            System.out.println("削除失敗");
-        }
+//         if (updateCount == 1) {
+//             System.out.println("削除成功");
+//         } else {
+//             System.out.println("削除失敗");
+//         }
 
-        statement.close();
-    }
-}
+//         statement.close();
+//     }
+// }
